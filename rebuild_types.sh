66c32e01c59b98c07133dcf8c0d5374b7265c1c6
Max Kasperowski <mka@informatik.uni-kiel.de>
2026-03-02 16:37:04 +0100

    update build command

--- rebuild_types.sh
+++ rebuild_types.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git clone https://github.com/kieler/klighd-vscode.git --depth 1
-datamodel-codegen --input klighd-vscode/schema/ --input-file-type jsonschema --output ./src/kieler_klighd_types --use-default
+datamodel-codegen --input klighd-vscode/schema/ --input-file-type jsonschema --output-model-type pydantic_v2.BaseModel --output ./src/kieler_klighd_types --use-default
 rm -rf klighd-vscode/
